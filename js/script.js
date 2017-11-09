9040de016784389f238f848f776c86421f6f280c
Augustine LF <c.r.allen310@gmail.com>
2017-11-08 16:15:51 -0800

    fixed prompt coding again

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 var name = prompt('what is your name?');
   console.log(name);
 
-var color = prompt('what is your favorite color?')
-switch (prompt) {
+var color = prompt('what is your favorite color?');
+console.log(color);
+switch (color) {
   case 'red':
     alert('what excellent taste you have!');
     break;
@@ -13,26 +14,26 @@ switch (prompt) {
     alert('it is indeed the best of all colors');
     break;
   case 'grey':
-    alert('elegant and simple!')
+    alert('elegant and simple!');
     break;
   case 'orange':
-    alert('it is a good food')
+    alert('it is a good food');
     break;
   case 'raspberry':
-    alert('an excellent choice i approve')
+    alert('an excellent choice i approve');
     break;
   case 'yellow':
-    alert('sunshine daisies butter mellow/turn this stupid fat rat yellow!')
+    alert('sunshine daisies butter mellow/turn this stupid fat rat yellow!');
     break;
   case 'black':
-    alert('v goffick')
+    alert('v goffick');
     break;
   case 'white':
-    alert('yes good')
+    alert('yes good');
     break;
   case 'mustard':
-    alert('do not ever talk to me again')
+    alert('do not ever talk to me again');
     break;
   default:
-    alert('your favorite color is incorrect')
+    alert('your favorite color is incorrect');
 }

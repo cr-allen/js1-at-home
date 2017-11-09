4f7e94c568d3e8b0e8ca8c093266d36944000770
Augustine LF <c.r.allen310@gmail.com>
2017-11-08 16:04:56 -0800

    fixed prompt

--- js/script.js
+++ js/script.js
@@ -1,29 +1,38 @@
 var name = prompt('what is your name?');
   console.log(name);
-var Prompt = require('what is your favorite color?', "blue - no yellooooow");
-var prompt = new Prompt({
-  name: 'colors',
-  message: 'What are your favorite colors?',
-  choices: [
-    'red',
-    'blue',
-    'yellow',
-    'chartruse',
-    'raspberry',
-    'gray',
-    'aqua',
-    'green',
-    'black',
-    'white',
-  ]
-});
 
-// // promises
-// prompt.run()
-//   .then(function(answers) {
-//     console.log(answers)
-//   })
-//   .catch(function(err) {
-//     console.log(err)
-//   })
-//
+var color = prompt('what is your favorite color?')
+switch (prompt) {
+  case 'red':
+    alert('what excellent taste you have!');
+    break;
+  case 'blue':
+  alert('a good color indeed');
+    break;
+  case 'chartruese':
+    alert('it is indeed the best of all colors');
+    break;
+  case 'grey':
+    alert('elegant and simple!')
+    break;
+  case 'orange':
+    alert('it is a good food')
+    break;
+  case 'raspberry':
+    alert('an excellent choice i approve')
+    break;
+  case 'yellow':
+    alert('sunshine daisies butter mellow/turn this stupid fat rat yellow!')
+    break;
+  case 'black':
+    alert('v goffick')
+    break;
+  case 'white':
+    alert('yes good')
+    break;
+  case 'mustard':
+    alert('do not ever talk to me again')
+    break;
+  default:
+    alert('your favorite color is incorrect')
+}

0eda807f9323c712c7f1245b048778cc61816dbe
Augustine LF <c.r.allen310@gmail.com>
2017-11-13 17:47:41 -0800

    stuff idr

--- js/script.js
+++ js/script.js
@@ -1,39 +1,39 @@
 var name = prompt('what is your name?');
   console.log(name);
 
-var color = prompt('what is your favorite color?', 'chartruese', 'mustard');
+var color = prompt('what is your favorite color?', 'chartruese');
 console.log(color);
 switch (color) {
   case 'red':
-    alert('what excellent taste you have!');
+    alert(', what excellent taste you have!');
     break;
   case 'blue':
-  alert('a good color indeed');
+    alert(name + ', that is a good color indeed');
     break;
   case 'chartruese':
-    alert('it is indeed the best of all colors');
+    alert(name + ', that is indeed the best of all colors');
     break;
   case 'grey':
-    alert('elegant and simple!');
+    alert(name + ', that elegant and simple!');
     break;
   case 'orange':
-    alert('it is a good food');
+    alert(name + ', orange is a good food');
     break;
   case 'raspberry':
-    alert('an excellent choice i approve');
+    alert(name + ', that is an excellent choice i approve');
     break;
   case 'yellow':
     alert('sunshine daisies butter mellow/turn this stupid fat rat yellow!');
     break;
   case 'black':
-    alert('v goffick');
+    alert(name + ', that is v goffick');
     break;
   case 'white':
     alert('yes good');
     break;
   case 'mustard':
-    alert('do not ever talk to me again');
+    alert(name + ', do not ever talk to me again');
     break;
   default:
-    alert('your favorite color is incorrect');
+    alert(name + ', your favorite color is incorrect');
 }

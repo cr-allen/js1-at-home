867938e2f9470d4a8a0cb7b46e3b61480e1986f0
Augustine LF <c.r.allen310@gmail.com>
2017-11-24 15:41:59 -0800

    wiped it

---
+++ js/prompt.js
@@ -0,0 +1,16 @@
+var prompt = new Prompt({
+  name: 'colors',
+  message: 'What is your favorite color?',
+  choices: [
+    'red',
+    'blue',
+    'yellow',
+    'chartruse',
+    'raspberry',
+    'gray',
+    'aqua',
+    'green',
+    'black',
+    'white',
+  ]
+});
--- js/script.js
+++ js/script.js
@@ -1,2 +1,26 @@
-var message = `I want to learn JS because it's a key language in web development and that's what I'd like to work in, mostly.`;
-console.log(message);
+var todo = [];
+for (let i; i <= 3; i++){
+  let todo = [];
+  var userTodo = prompt('what do you need to do this weekend','take many naps');
+  var dueDate = prompt('how many days should each thing take', '1')
+  dueDate = parseInt(dueDate)
+  if(dueDate == NaN){
+    alert('please use numbers (eg, 1 instead of one or dates)')
+  } else {
+    todo.push(userTodo, deuDate)
+  }
+  todo.push(userTodo);
+  if (todo.length == 3) {
+    break; //<-- cleanest way to stop the loop
+  }
+  else {
+    continue;
+  }
+}
+let v = 0;
+while (v <= 2) {
+  if (todo[v][1] < todo[v - 1][1]) {
+
+  }
+  math.max(todo[v][1])
+}

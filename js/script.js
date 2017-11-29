5aea644d120a612fa0660a974153b9c529935494
Augustine LF <c.r.allen310@gmail.com>
2017-11-29 15:47:44 -0800

    most of day7 hw

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
-var message = `I want to learn JS because it's a key language in web development and that's what I'd like to work in, mostly.`;
-console.log(message);
+var body = querySelector('body');
+
+var h1 = console.log('give me a title!');
+var title = Document.creatElement('h1');
+title.textContent = h1;
+body.appendChild(title);
+title.setAttribute('align', 'center');
+
+var para = console.log('write a few sentences about what you did today! :)');
+var text = Document.creatElement('p');
+text.textContent = para;
+title.appendChild(text);
+
+var color = console.log('whats your favorite color?', 'blue, no yellooooooow');
+var bkgrnd = document.body.style.backgroundColor = color;
+body.appendChild = backgroundColor;
+
+//
